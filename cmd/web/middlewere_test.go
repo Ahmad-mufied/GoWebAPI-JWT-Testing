e40ad10a2d54e9eb301fdc7ecafc8c8e44d94e5a
Ahmad-mufied <teranggelap01@gmail.com>
2023-06-01 16:05:42 +0700

    40. Setting up a test enviroment with testing.M

--- cmd/web/middlewere_test.go
+++ cmd/web/middlewere_test.go
@@ -8,8 +8,6 @@ import (
 	"testing"
 )
 
-var app application
-
 func Test_application_addIPToContext(t *testing.T) {
 	tests := []struct {
 		headerName  string
@@ -62,8 +60,6 @@ func Test_application_addIPToContext(t *testing.T) {
 }
 
 func Test_application_ipFromContext(t *testing.T) {
-	// create an app variable of type application
-	var app application
 
 	// get a context
 	var ctx = context.Background()
---
+++ cmd/web/setup_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+var app application
+
+func TestMain(m *testing.M) {
+	
+
+	os.Exit(m.Run())
+
+}
