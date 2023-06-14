01b078b1a72a8d8489884c4ca2388e50a4313640
Ahmad-mufied <teranggelap01@gmail.com>
2023-06-15 00:34:36 +0700

    50. Setting up a database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/alexedwards/scs/v2"
 )
 
-
 type application struct {
+	DSN     string
+	DB      *sql.DB
 	Session *scs.SessionManager
 }
 
@@ -16,15 +19,25 @@ func main() {
 	// set up an app config
 	app := application{}
 
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
+
+	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
+
 	// get a session mamager
-	app.Session = getSession() 
+	app.Session = getSession()
 
 	// print out a message
 	log.Println("Starting server on port 8080...")
 
-
 	// start the server
-	err := http.ListenAndServe(":8080", app.routes())
+	err = http.ListenAndServe(":8080", app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
