4d832f1b0284678287f0d954e7a6da149f54c27c
Ahmad-mufied <teranggelap01@gmail.com>
2023-09-25 10:19:38 +0700

    74. Testing AllUsers

--- pkg/repository/dbrepo/users_postgres_test.go
+++ pkg/repository/dbrepo/users_postgres_test.go
@@ -141,3 +141,35 @@ func TestPostgresDBRepo_InsertUser(t *testing.T) {
 		t.Errorf("inser user returned wrong id; but got %d", id)
 	}
 }
+
+func TestPostgresDBRepo_AllUsers(t *testing.T) {
+	users, err := testRepo.AllUsers()
+	if err != nil {
+		t.Errorf("all users reports an error: %s", err)
+	}
+
+	if len(users) != 1 {
+		t.Errorf("all users reports wrong size; expected 1, but got %d ", len(users))
+	}
+
+	testUser := data.User{
+		FirstName: "Jack",
+		LastName:  "Smith",
+		Email:     "admin@example.com",
+		Password:  "secret",
+		IsAdmin:   1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	_, _ = testRepo.InsertUser(testUser)
+
+	users, err = testRepo.AllUsers()
+	if err != nil {
+		t.Errorf("all users reports an error: %s", err)
+	}
+
+	if len(users) != 2 {
+		t.Errorf("all users reports worng size after insert : expected 2, but got %d", len(users))
+	}
+}
