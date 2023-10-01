5a2e351efa31005795063e4a45d75d23a855563f
Ahmad-mufied <teranggelap01@gmail.com>
2023-10-01 11:25:14 +0700

    91. Setting up an api with our existing code base

---
+++ cmd/api/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"webapp/pkg/repository"
+	"webapp/pkg/repository/dbrepo"
+)
+
+const port = 8090
+
+type application struct {
+	DSN       string
+	DB        repository.DatabaseRepo
+	Domain    string
+	JWTSecret string
+}
+
+func main() {
+	var app application
+	flag.StringVar(&app.Domain, "domain", "example.com", "Domain for application, e.g. company.com")
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres sslmode=disable dbname=users timezone=UTC connect_timeout=5", "Postgres connection")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "\n\neyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NjEzMzc1NywiaWF0IjoxNjk2MTMzNzU3fQ.kVSyCE6TeFo8TFLGhBStBLzKpwPh75lr97EisMWHEEU", "signing secret")
+	flag.Parse()
+
+	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+
+	log.Printf("Starting api on port %d\n", port)
+
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
