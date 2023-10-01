81a006d1be477227b84e1eec4f257d3ab71b1c4d
Ahmad-mufied <teranggelap01@gmail.com>
2023-10-01 23:05:37 +0700

    99. Setting up a simple program to generate tokens for testing

--- cmd/api/setup_test.go
+++ cmd/api/setup_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 var app application
+var expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZXhhbXBsZS5jb20iLCJleHAiOjE2OTU4MTYxOTYsImlzcyI6ImV4YW1wbGUuY29tIiwibmFtZSI6IkpvaG4gRG9lIiwic3ViIjoiMSJ9.g_y6XDPSSCy4ByjMszszCoO_C81kYtiUK_3dkAQl0F8"
 
 func TestMain(m *testing.M) {
 	app.DB = &dbrepo.TestDBRepo{}
