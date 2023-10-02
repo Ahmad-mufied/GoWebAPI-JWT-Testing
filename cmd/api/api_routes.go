2b06c05f9c73bebd08a2a92e7081ea2ca803ea8c
Ahmad-mufied <teranggelap01@gmail.com>
2023-10-02 10:24:05 +0700

    104. Add middleware to routes

--- cmd/api/api_routes.go
+++ cmd/api/api_routes.go
@@ -11,30 +11,30 @@ func (app *application) routes() http.Handler {
 
 	// register middleware
 	mux.Use(middleware.Recoverer)
-
-	// mux.Use(app.enableCORS)
+	mux.Use(app.enableCORS)
 
 	// authentication routes - auth handler, refresh
 	mux.Post("/auth", app.authenticate)
 	// curl http://localhost:8090/auth -X POST -H "Content-Type: application/json" -d '{"email":"admin@example.com","password":"secret"}'
 	mux.Post("/refresh-token", app.refresh)
 
-	// test handler
-	mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		var payload = struct {
-			Message string `json:"message"`
-		}{
-			Message: "hello, world",
-		}
-
-		_ = app.writeJSON(w, http.StatusOK, payload)
-
-		// curl http://localhost:8090/test
-	})
+	//// test handler
+	//mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+	//	var payload = struct {
+	//		Message string `json:"message"`
+	//	}{
+	//		Message: "hello, world",
+	//	}
+	//
+	//	_ = app.writeJSON(w, http.StatusOK, payload)
+	//
+	//	// curl http://localhost:8090/test
+	//})
 
 	// protected routes
 	mux.Route("/users", func(mux chi.Router) {
 		// use auth middleware
+		mux.Use(app.authRequired)
 
 		mux.Get("/", app.allUsers)
 		mux.Get("/{userID}", app.getUser)
